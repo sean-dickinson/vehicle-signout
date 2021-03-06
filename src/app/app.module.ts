acee10e351107c18b2d20dbf2fb2a210b6c230ba
atinybeardedman <sedickinson9123@gmail.com>
2021-03-06 11:50:15 -0500

    refactor: continued to move components to lazy modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,17 @@ import { AngularFirestoreModule } from '@angular/fire/firestore'
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { AddSignoutDialogComponent } from './signout-view/add-signout-dialog/add-signout-dialog.component';
 import { LoginComponent } from './core/login/login.component';
 import { SelectVehicleComponent } from './core/select-vehicle/select-vehicle.component';
-import { ManageSignoutsComponent } from './manage-signout/manage-signouts/manage-signouts.component';
-import { ConfirmDialogComponent } from './manage-signout/confirm-dialog/confirm-dialog.component';
 import { NavListComponent } from './core/nav-list/nav-list.component';
-import { UriEncodedPipe } from './uri-encoded.pipe';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddSignoutDialogComponent,
     LoginComponent,
     SelectVehicleComponent,
-    ManageSignoutsComponent,
-    ConfirmDialogComponent,
     NavListComponent,
-    UriEncodedPipe,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +28,6 @@ import { SharedModule } from './shared/shared.module';
     AngularFireAuthModule,
     SharedModule
   ],
-  entryComponents: [AddSignoutDialogComponent, ConfirmDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/uri-encoded.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UriEncodedPipe } from './uri-encoded.pipe';
-
-describe('UriEncodedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UriEncodedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

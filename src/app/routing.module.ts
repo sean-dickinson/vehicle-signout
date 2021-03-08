0335da419bdbdedd1b958d3d997d0a42cef8e191
atinybeardedman <sedickinson9123@gmail.com>
2021-03-08 10:47:14 -0500

    feat: added initial router guard

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SelectVehicleComponent } from './core/select-vehicle/select-vehicle.component';
 import { LoginComponent } from './core/login/login.component';
-
+import { redirectUnauthorizedTo, canActivate } from '@angular/fire/auth-guard';
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 const routes:Routes = [
-  {path: '', component: SelectVehicleComponent},
+  {path: '', component: SelectVehicleComponent, ...canActivate(redirectUnauthorizedToLogin)},
   {path: 'vehicle', loadChildren: () => import('./signout-view/signout-view.module').then(m => m.SignoutViewModule)},
   {path:'my-signouts', loadChildren: () => import('./manage-signout/manage-signout.module').then(m => m.ManageSignoutModule)},
   {path: 'login', component: LoginComponent}

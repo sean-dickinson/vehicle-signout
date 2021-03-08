64b28d77ed58a39cbb28d8253341690f09110f1a
atinybeardedman <sedickinson9123@gmail.com>
2021-03-08 10:47:37 -0500

    feat: updated user service for firestore

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { VehicleUser } from './models/vehicle-user';
-import { BehaviorSubject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private _user: BehaviorSubject<VehicleUser> = new BehaviorSubject<VehicleUser>(null);
+  private _user: ReplaySubject<VehicleUser> = new ReplaySubject<VehicleUser>(1);
   constructor(private af: AngularFirestore) { }
 
-  getUser(): BehaviorSubject<VehicleUser> {
+  getUser(): Observable<VehicleUser> {
     return this._user;
   }
 
   setUser(uid: string): Promise<void>{
-    return this.af.doc(`/users/${uid}`).valueChanges().toPromise()
-      .then((user: VehicleUser) => {
+    return this.af.doc(`/users/${uid}`).get().toPromise()
+      .then((snap) => {
+        const user = snap.data() as VehicleUser;
         this._user.next(user);
       });
   }

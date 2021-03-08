4b57d6225393a036e7fc97d3b6317428cf8c457b
atinybeardedman <sedickinson9123@gmail.com>
2021-03-08 13:27:11 -0500

    feat: removed unnecessary get() call for user

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@angular/core';
 import { VehicleUser } from './models/vehicle-user';
 import { Observable, ReplaySubject } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private _user: ReplaySubject<VehicleUser> = new ReplaySubject<VehicleUser>(1);
-  constructor(private af: AngularFirestore) { }
+  constructor(private af: AngularFirestore) { 
+    this._user.next(null);
+  }
 
   getUser(): Observable<VehicleUser> {
     return this._user;
   }
 
   setUser(uid: string): Promise<void>{
-    return this.af.doc(`/users/${uid}`).get().toPromise()
-      .then((snap) => {
-        const user = snap.data() as VehicleUser;
+    return this.af.doc(`/users/${uid}`).valueChanges().pipe(take(1)).toPromise()
+      .then((user: VehicleUser) => {
         this._user.next(user);
       });
   }

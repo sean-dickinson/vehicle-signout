9c7aa2cd91807c6891f0c8e87d56ac8c8c9a2396
atinybeardedman <sedickinson9123@gmail.com>
2021-03-23 18:53:14 -0400

    refactor: added sub folder

--- src/app/signout-view/signout-view.module.ts
+++ src/app/signout-view/signout-view.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ViewSignoutsByVehicleComponent } from './view-signouts-by-vehicle/view-signouts-by-vehicle.component';
-import { VehicleSignoutCardComponent } from './vehicle-signout-card/vehicle-signout-card.component';
+import { VehicleSignoutCardComponent } from './components/vehicle-signout-card/vehicle-signout-card.component';
 import { SignoutViewRoutingModule } from './signout-view-routing.module';
-import { LastSignoutComponent } from './last-signout/last-signout.component';
-import { AddSignoutDialogComponent } from './add-signout-dialog/add-signout-dialog.component';
-import { AddSignoutStepperComponent } from './add-signout-stepper/add-signout-stepper.component';
+import { LastSignoutComponent } from './components/last-signout/last-signout.component';
+import { AddSignoutDialogComponent } from './components/add-signout-dialog/add-signout-dialog.component';
+import { AddSignoutStepperComponent } from './components/add-signout-stepper/add-signout-stepper.component';
 
 
 

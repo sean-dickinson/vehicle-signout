232b78a23a347893f8379bc01a1790443b044fe9
atinybeardedman <sedickinson9123@gmail.com>
2021-03-23 18:53:35 -0400

    feat: added helper time functions

--- src/app/utilities/functions.ts
+++ src/app/utilities/functions.ts
@@ -1,5 +1,19 @@
+import { formatDate } from "@angular/common";
 import { SimpleEntity } from "app/models/simple-entity";
 
 export function compareUID(o1: SimpleEntity, o2: SimpleEntity): boolean {
-    return o1.uid === o2.uid;
+  return o1.uid === o2.uid;
+}
+
+export function combineDateTime(date: Date, time: string): Date {
+  const resultDate = new Date(date);
+  const hours = parseInt(time.substr(0, 2), 10);
+  const minutes = parseInt(time.substr(3, 2), 10);
+  resultDate.setHours(hours);
+  resultDate.setMinutes(minutes);
+  return resultDate;
+}
+
+export function getTimestring(date: Date): string {
+    return formatDate(date, 'HH:MM', 'en');
 }

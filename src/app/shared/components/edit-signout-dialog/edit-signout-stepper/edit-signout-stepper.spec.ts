95152754738252c9b66052a3fefa9abb9edb6111
atinybeardedman <sedickinson9123@gmail.com>
2021-04-07 14:04:06 -0400

    tests: migrated stepper to spectator testing

--- src/app/shared/components/edit-signout-dialog/edit-signout-stepper/edit-signout-stepper.spec.ts
+++ src/app/shared/components/edit-signout-dialog/edit-signout-stepper/edit-signout-stepper.spec.ts
@@ -1,59 +1,52 @@
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { signoutData } from 'testing-helpers/testing-data/test-signout-data';
-import { testVehicles } from 'testing-helpers/testing-data/test-vehicle-data';
 
-import { HarnessLoader } from '@angular/cdk/testing';
-import { MatSelectHarness } from '@angular/material/select/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { FormBuilder } from "@angular/forms";
+import { signoutData } from "testing-helpers/testing-data/test-signout-data";
+import { testVehicles } from "testing-helpers/testing-data/test-vehicle-data";
 
+import { EditSignoutStepperComponent } from "./edit-signout-stepper.component";
+import {
+  byTextContent,
+  createComponentFactory,
+  Spectator,
+} from "@ngneat/spectator";
+import { MockModule } from "ng-mocks";
 
-import { EditSignoutStepperComponent } from './edit-signout-stepper.component';
+import { SignoutDataService } from "app/signout-data.service";
 
+import { SharedModule } from "app/shared/shared.module";
 
-describe('AddSignoutStepperComponent', () => {
-  let loader: HarnessLoader;
-  let component: EditSignoutStepperComponent;
-  let fixture: ComponentFixture<EditSignoutStepperComponent>;
-  let el: DebugElement;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [EditSignoutStepperComponent],
-      providers: [FormBuilder],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
+fdescribe("EditSignoutStepperComponent", () => {
+  let spectator: Spectator<EditSignoutStepperComponent>;
+  const createComponent = createComponentFactory({
+    component: EditSignoutStepperComponent,
+    imports: [MockModule(SharedModule)],
+    providers: [FormBuilder],
+    mocks: [SignoutDataService],
+  });
+  beforeEach(() => (spectator = createComponent()));
 
-    fixture = TestBed.createComponent(EditSignoutStepperComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    el = fixture.debugElement;
-    loader = TestbedHarnessEnvironment.loader(fixture);
+  it("should render the supplied vehicles", () => {
+    spectator.setInput({
+      vehicles: testVehicles,
+      signout: signoutData[0],
+    });
+    expect(
+      spectator.queryAll(
+        byTextContent(testVehicles.join(","), {
+          selector: "mat-select mat-option",
+        })
+      )
+    ).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should render the supplied vehicles', () => {
-    // TODO: use test harnesses when the select actually works?!
-    component.vehicles = testVehicles;
-    component.signout = signoutData[0];
-    fixture.detectChanges();
-    const options = el
-      .query(By.css('mat-select'))
-      .children.map((node) => node.nativeElement.textContent);
-    expect(testVehicles.map((v) => v.name)).toEqual(options);
-  });
-
-  it('should select the vehicle supplied in the signout input', async () => {
-    component.ngOnInit();
-    component.vehicles = testVehicles;
-    component.signout = signoutData[0];
-    component.ngOnChanges();
-    fixture.detectChanges();
-    const selectedVehicle = component.generalStepGroup.get('vehicleCtrl').value;
+  it("should select the vehicle supplied in the signout input", () => {
+    spectator.setInput({
+      vehicles: testVehicles,
+      signout: signoutData[0],
+    });
+    const selectedVehicle = spectator.component.generalStepGroup.get(
+      "vehicleCtrl"
+    ).value;
     expect(selectedVehicle.uid).toEqual(signoutData[0].vehicleID);
-})
+  });
 });

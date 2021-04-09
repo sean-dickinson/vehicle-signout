b68a6d4b5a92cb69c9fe85464a6327d5160154cd
atinybeardedman <sedickinson9123@gmail.com>
2021-04-09 17:12:37 -0400

    fix: add explicit past columns to manage card

--- src/app/manage-signouts/manage-signouts-card/manage-signouts-card.component.ts
+++ src/app/manage-signouts/manage-signouts-card/manage-signouts-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { VehicleSignout } from 'app/models/vehicle-signout';
-import { VehicleUser } from 'app/models/vehicle-user';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { VehicleSignout } from "app/models/vehicle-signout";
+import { VehicleUser } from "app/models/vehicle-user";
 
 @Component({
-  selector: 'app-manage-signouts-card',
-  templateUrl: './manage-signouts-card.component.html',
-  styleUrls: ['./manage-signouts-card.component.css']
+  selector: "app-manage-signouts-card",
+  templateUrl: "./manage-signouts-card.component.html",
+  styleUrls: ["./manage-signouts-card.component.css"],
 })
 export class ManageSignoutsCardComponent implements OnInit {
   @Input() signouts: VehicleSignout[];
@@ -14,15 +14,13 @@ export class ManageSignoutsCardComponent implements OnInit {
   @Input() time: string;
   @Output() edit = new EventEmitter<VehicleSignout>();
   @Output() remove = new EventEmitter<VehicleSignout>();
-  currentColumns = ['reason', 'startTime', 'endTime', 'actions']
+  currentColumns = ["reason", "vehicleName", "startTime", "endTime", "actions"];
+  pastColumns = ["reason", "vehicleName", "startTime", "endTime"];
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   get columns(): string[] {
-    return this.type === 'Current' ? this.currentColumns : [];
+    return this.type === "Current" ? this.currentColumns : this.pastColumns;
   }
-
-
 }
